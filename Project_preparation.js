a8a399c08a6b6060e6e9d1f2c3e64dfe57d7bf5a
Strahil731 <strahil1998@abv.bg>
2024-12-26 14:11:05 +0200

    Create Project_preparation.js

---
+++ Project_preparation.js
@@ -0,0 +1,7 @@
+function projectPreparation(name, project) {
+    let hours = project * 3;
+
+    console.log(`The architect ${name} will need ${hours} hours to complete ${project} project/s.`);
+}
+
+projectPreparation("Sanya", 9)
