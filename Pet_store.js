02fe5c117fb711d2fff62a851297b9c8a9d9ffcf
Strahil731 <strahil1998@abv.bg>
2024-12-26 14:07:36 +0200

    Create Pet_store.js

---
+++ Pet_store.js
@@ -0,0 +1,9 @@
+function petStore(dogFood, catFood){
+    let dogFoodPrice = dogFood * 2.50;
+    let catFoodPrice = catFood * 4;
+    let sum = dogFoodPrice + catFoodPrice;
+
+    console.log(`${sum} lv.`)
+}
+
+petStore(13, 9)
